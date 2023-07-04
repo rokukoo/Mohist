52f3024ed118b4ede363fb4bd5cb5e2763c8ebce
rabbit0w0 <rabbit0w0@outlook.com>
2023-07-04 23:08:48 +0800

    try add license in arguments

--- src/main/java/org/safebabe/mohistpro/LicenseConfig.java
+++ src/main/java/org/safebabe/mohistpro/LicenseConfig.java
@@ -1,5 +1,7 @@
 package org.safebabe.mohistpro;
 
+import net.minecraftforge.server.ServerMain;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -9,7 +11,13 @@ public class LicenseConfig {
 
     public static File license = new File("neptuneX/LICENSE.bin");
 
+    public static String licenseCode = null;
+
     public static String getLicense() {
+        if (licenseCode != null) return licenseCode;
+        ServerMain.mainArgs.forEach(i -> {
+            if (i.startsWith("--license")) licenseCode = i.substring(9);
+        });
         if (!license.exists()) return "";
         try {
             return Files.readString(license.toPath(), Charset.defaultCharset()).trim();
